b20c7cd81d99d577055fca592c24eea76fa11c01
SchadenKai <kairutecsonpersonal@gmail.com>
2025-11-26 16:09:45 +0800

    fix: revert postgres_password to default value for security reasons

--- fullstack/backend/app/core/config.py
+++ fullstack/backend/app/core/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     postgres_user: str = os.getenv("POSTGRES_USER", "postgres")
-    postgres_password: str = os.getenv("POSTGRES_PASSWORD", "FodunXwrPVyXfeTz")
+    postgres_password: str = os.getenv("POSTGRES_PASSWORD", "changethis")
     postgres_host: str = os.getenv("POSTGRES_HOST", "localhost")
     postgres_port: int = int(os.getenv("POSTGRES_PORT", 5432))
     postgres_db: str = os.getenv("POSTGRES_DB", "postgres")
